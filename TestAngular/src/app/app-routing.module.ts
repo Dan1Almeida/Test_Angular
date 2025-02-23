56b07535320a5a517fdaa38e02ffaa53957ae92b
Dan1Almeida <daniel.dnlsilva7@gmail.com>
2025-02-23 13:24:10 -0300

    Header

--- TestAngular/src/app/app-routing.module.ts
+++ TestAngular/src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { CadastrarPessoaComponent } from './pages/pessoas/cadastrar-pessoa/cadastrar-pessoa.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: HomeComponent },  // Página inicial
+  { path: 'pessoas/cadastro', component: CadastrarPessoaComponent }  // Página de cadastro
+];
+
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- TestAngular/src/app/app.module.ts
+++ TestAngular/src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { HomeComponent } from './pages/home/home.component';
+import { CadastrarPessoaComponent } from './pages/pessoas/cadastrar-pessoa/cadastrar-pessoa.component';
+import { EditarPessoaComponent } from './pages/pessoas/editar-pessoa/editar-pessoa.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    NavbarComponent,
+    HomeComponent,
+    CadastrarPessoaComponent,
+    EditarPessoaComponent
   ],
   imports: [
     BrowserModule,
